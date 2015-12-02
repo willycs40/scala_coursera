99cd4938b1636957078accc1c1a0b20ba766f679
Will CS <will@apressci.co.uk>
2015-12-02 14:54:17 +0000

    adding week 1 complete exercises

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -14,15 +14,41 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = 
+    if ((c==0) && (r==0)) 1 else
+      if ((c<0) || (c>r)) 0 else 
+        pascal(c-1, r-1) + pascal(c,r-1)
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+        def balance_count(bracket_count: Int, chars: List[Char]): Boolean = {
+          if (bracket_count <0) false else
+          if (chars.isEmpty)
+            if (bracket_count == 0) true else false 
+          else {
+            val first = chars.head
+            if (first == '(') balance_count(bracket_count + 1, chars.tail) 
+            else
+              if (first == ')') balance_count(bracket_count - 1, chars.tail) 
+              else balance_count(bracket_count, chars.tail)
+          }
+        }
+        
+        balance_count(0, chars)
+          
+      }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if(money == 0)
+      1
+    else if(money > 0 && !coins.isEmpty)
+      countChange(money - coins.head, coins) + countChange(money, coins.tail)
+    else
+      0
+  }
 }
